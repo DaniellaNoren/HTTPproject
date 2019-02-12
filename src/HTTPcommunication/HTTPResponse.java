d42395d98ebe6acba53a9990fb2f8dbb3045e6dc
Daniella Noren <daniellanoren@live.se>
2019-02-12 15:13:08 +0100

    Make basic method for generating HTTP-Responses in HTTPResponse

--- src/HTTPcommunication/HTTPResponse.java
+++ src/HTTPcommunication/HTTPResponse.java
@@ -1,6 +1,21 @@
 package HTTPcommunication;
 
+import java.io.File;
+import java.util.Date;
+
 public class HTTPResponse {
 
+    //Basic idea of how a HTTPresponse-generator might look like. Need to rethink how to add the body, though
+    public static String getHTTPResponse(int status, String msg, String type, File body){
+        StringBuilder sb = new StringBuilder();
+        sb.append("HTTP/1.1").append("\t").append(status).append("\t").append(msg).append("\n")
+        .append("Date:").append("\t").append(new Date()).append("\n")
+        .append("Server: HttpServer").append("\n")
+        .append("Content-Type:").append("\t").append(type).append("\n")
+        .append("Content-Length:").append("\t").append(body.length()).append("\n\n")
+        .append(body);
+
+        return sb.toString();
+    }
 
 }
