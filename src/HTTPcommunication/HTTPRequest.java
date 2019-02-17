19b6ac977dbcb0895656efd3db39e533b5415f46
Daniella Noren <daniellanoren@live.se>
2019-02-17 15:14:17 +0100

    Create more fields and constructors for HTTPRequest and getters

--- src/HTTPcommunication/HTTPRequest.java
+++ src/HTTPcommunication/HTTPRequest.java
@@ -2,16 +2,56 @@ package HTTPcommunication;
 
 public class HTTPRequest {
 
-    private String method;
-    private String contentType;
-    private String URL;
+    private final String HTTP_VERSION = "HTTP/1.1";
 
-    public HTTPRequest(String method, String contentType, String URL){
+    private String method;
+    private String URL;
+    private String connection;
+    private String host;
+
+    private int contentLength;
+    private String contentType;
+    private byte[] body;
+
+    public HTTPRequest(String method, String URL, String connection, String host){
+        this(method, URL, connection, host, "", 0, null);
+    }
+    public HTTPRequest(String method, String URL, String connection, String host, String contentType, int contentLength, byte[] body){
         this.method = method;
-        this.contentType = contentType;
         this.URL = URL;
+        this.connection = connection;
+        this.host = host;
+        this.contentType = contentType;
+        this.contentLength = contentLength;
+        this.body = body;
     }
 
-    //getters/setters
-    //toString
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String getURL() {
+        return URL;
+    }
+
+    public String getConnection() {
+        return connection;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public int getContentLength() {
+        return contentLength;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public byte[] getBody() {
+        return body;
+    }
 }
