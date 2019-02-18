89f0e693a4d91b074f9726b40c8215632a827bba
Daniella Noren <daniellanoren@live.se>
2019-02-18 11:37:08 +0100

    Create HTTPResponse-class

--- src/main/java/HTTPcommunication/HTTPResponse.java
+++ src/main/java/HTTPcommunication/HTTPResponse.java
@@ -1,21 +1,55 @@
 package HTTPcommunication;
 
-import java.io.File;
-import java.util.Date;
-
 public class HTTPResponse {
 
-    //Basic idea of how a HTTPresponse-generator might look like. Need to rethink how to add the body, though
-    public static String getHTTPResponse(int status, String msg, String type, File body){
-        StringBuilder sb = new StringBuilder();
-        sb.append("HTTP/1.1").append("\t").append(status).append("\t").append(msg).append("\n")
-        .append("Date:").append("\t").append(new Date()).append("\n")
-        .append("Server: HttpServer").append("\n")
-        .append("Content-Type:").append("\t").append(type).append("\n")
-        .append("Content-Length:").append("\t").append(body.length()).append("\n\n")
-        .append(body);
+   private final String HTTP_VERSION = "HTTP/1.1";
 
-        return sb.toString();
+   private int status;
+   private String version;
+   private String message;
+   private String contentType;
+   private int contentLength;
+   private String connection;
+   private byte[] body;
+
+    public int getStatus() {
+        return status;
     }
 
+    public String getVersion() {
+        return version;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public int getContentLength() {
+        return contentLength;
+    }
+
+    public String getConnection() {
+        return connection;
+    }
+
+    public byte[] getBody() {
+        return body;
+    }
+
+    public String getHTTP_VERSION(){
+        return HTTP_VERSION;
+    }
+    @Override
+    public String toString() {
+        String s = HTTP_VERSION+" "+status+" "+message+"\n"+
+                    "Content-Type: "+contentType+"\n"+
+                    "Content-Length: "+contentLength+"\n"+
+                    "Connection: "+connection+"\n"+
+                    "Version: "+version+"\n\n";
+        return s;
+    }
 }
