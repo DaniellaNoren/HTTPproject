0adb8aa703cdc0488849c6d3cecd739c9af3d900
Daniella Noren <daniellanoren@live.se>
2019-02-18 19:49:07 +0100

    Clean up HTTPResponseGenerator a bit

--- src/main/java/HTTPcommunication/HTTPResponseGenerator.java
+++ src/main/java/HTTPcommunication/HTTPResponseGenerator.java
@@ -7,60 +7,51 @@ import java.nio.file.Files;
 public class HTTPResponseGenerator {
 
     public static HTTPResponse getHTTPResponse(HTTPRequest request){
+
         HTTPResponse response = null;
         String message = "OK";
         String contentType = "";
         int status = 200;
         int contentLength = 0;
+
+        String url = "";
+        if(!(request.getURL().equals("/") && request.getURL().isEmpty()))
+         url = request.getURL().substring(1);
+
         String path = ".//web";
-        String URL2 = request.getURL();
-        URL2 = URL2.substring(1);
-        System.out.println("url:"+request.getURL());
-        if(!request.getURL().isEmpty())
-            if(request.getURL().contains(".")) {
-                String url = request.getURL().substring(request.getURL().indexOf("."));
-                contentType = contentTypeRequested(url);
+
+        if(!request.getURL().isEmpty() && request.getURL().contains(".")){
+                String b = request.getURL().substring(request.getURL().indexOf("."));
+                contentType = contentTypeRequested(b);
                 System.out.println(contentType);
             }
+
         File file = null;
-        byte[] content = new byte[request.getContentLength()];
-        if((!(request.getURL().equals("/"))) && new File(path, URL2).exists()) {
-            System.out.println("kjsdksddkfj"+URL2);
-            file = new File(path, URL2);
-            //content = new FileToBytesConverter().convertFileToBytes(file);
+        byte[] content = new byte[0];
+
+        if((!(request.getURL().equals("/"))) && new File(path, url).exists()) {
+            file = new File(path, url);
+        }else if(request.getURL().equals("/")){
+             file = new File(path, "index.html");
+             contentType = "text/html";
+        }
+        else {
+            status = 404;
+            message = "Not Found";
+            file = new File(path, "404.html");
+        }
+
+
             try {
                 content = Files.readAllBytes(file.toPath());
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            contentLength = content.length;
-        }else if(request.getURL().equals("/")){
-            try {
-                content = Files.readAllBytes(new File(path+"/index.html").toPath());
-                contentType = "text/html";
-                System.out.println("whwhwhwh"+path);
-                contentLength = content.length;
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        else {
-            status = 404;
-            message = "Not Found";
-            try {
-                File f = new File(path+"/404.html");
-                //String path = f.getAbsolutePath();
-                //System.out.println(path);
-                content = Files.readAllBytes((f).toPath());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+
+        contentLength = content.length;
         String connection = request.getConnection();
+
         response = new HTTPResponse(status, message, contentType, contentLength, connection, content);
-
-
-
 
         return response;
     }
