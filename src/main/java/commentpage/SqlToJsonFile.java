cc353c370458c39e6394513202227635d73846d8
Ida <ida.fagerlund@hotmail.com>
2019-02-20 11:23:07 +0100

    Rename file

--- src/main/java/commentpage/SqlToJsonFile.java
+++ src/main/java/commentpage/SqlToJsonFile.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class JsonParser {
+public class SqlToJsonFile {
 
     public void writeJsonToFile(List<String> messages){
 
