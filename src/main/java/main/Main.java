5e2ea1d121241905822e514998cc3996e4f7b642
Elin <elin.sexton@iths.se>
2019-02-20 13:07:38 +0100

    Added instance of SQLDatabase in Main.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,11 +1,14 @@
 package main;
 
+import storage.SQLDatabase;
+
 import java.net.ServerSocket;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        SQLDatabase db = new SQLDatabase();
         Server server = new Server(8081);
         server.startServer();
 
