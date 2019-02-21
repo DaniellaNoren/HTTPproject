b1aa14cacfa51cdb8511f72ed6425c34418eeb5d
anton <anton.blixter@gmail..com>
2019-02-21 10:11:29 +0100

    Insert call for url handler

--- src/main/java/HTTPcommunication/Client.java
+++ src/main/java/HTTPcommunication/Client.java
@@ -28,24 +28,13 @@ public class Client extends Thread{
         this.in = in;
     }
 
-    private void specificUrlHandler(String url) {
-        switch (url) {
-            case "/URL.html":
-                if (request.getQuery().length() > 0){ //Create json file if URL page contains parameters
-                    QueryStringToJSON.convert(new File("web/jsonFromQuery.json"), request.getQuery());
-                }
-                break;
-                //Add more cases
-            default:
-                break;
-        }
-    }
-
     public void run() {
 
                 try {
                        request = HTTPRequestFactory.getHTTPRequest(getRequestAsList());
                        request.setBody(getBody(request.getContentLength()));
+
+                       specificUrlHandler(request.getQuery());
 
                         //Till kommentar sidan
                         if(request.getMethod().equals("POST")){
@@ -92,6 +81,18 @@ public class Client extends Thread{
 
     }
 
+    private void specificUrlHandler(String url) {
+        switch (url) {
+            case "/URL.html":
+                if (request.getQuery().length() > 0){ //Create json file if URL page contains parameters
+                    QueryStringToJSON.convert(new File("web/jsonFromQuery.json"), request.getQuery());
+                }
+                break;
+            //Add more cases
+            default:
+                break;
+        }
+    }
 
     public List<String> getRequestAsList() throws IOException {
         List<String> requestList = new ArrayList<>();
