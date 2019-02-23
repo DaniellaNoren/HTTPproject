dd572b9cdaa42661498a0370fa4c608f6766edee
Ida <ida.fagerlund@hotmail.com>
2019-02-23 13:05:20 +0100

    Create all files for DisplayData plugin

--- src/main/java/plugin/displayDataPlugin/DisplayData.java
+++ src/main/java/plugin/displayDataPlugin/DisplayData.java
@@ -5,6 +5,9 @@ import HTTPcommunication.HTTPResponse;
 import plugin.PluginAnnotationPage;
 import plugin.interfaces.PageService;
 
+import java.io.File;
+import java.net.URL;
+
 /**
  * This plugin leads to a page with a visual representation of the statistics that are saved with the "GetData" plugin.
  * It's a separate plugin so it can be a choice whether the statistics should only be stored or also displayed.
@@ -15,10 +18,24 @@ public class DisplayData implements PageService {
 
     @Override
     public HTTPResponse response(HTTPRequest httpRequest) {
-        return null;
+
+        URL url = getClass().getResource("Statistics.html");
+        File html = new File(url.getPath());
+        //String html =
+
+//                "<!DOCTYPE html><html><h3 style='color:red;'>HELLO "+"heeeej"+"</h3>"+"<" +
+//                "<form method='GET'><input type='text' name='name'/><input type='submit'/></form></html>";
+
+
+        byte[] body = html.getBytes();
+        HTTPResponse response = new HTTPResponse();
+        response.setStatus(200).setMessage("OK").setContentType("text/html").setContentLength(body.length).setBody(body);
+
+        return response;
     }
 
-    //Vet fortfarande bara hur man läser från en jsonfil från javascript så löser det på samma sätt som kommentarsidan...
+
+    //Vet fortfarande bara hur man läser från en jsonfil från javascript så löser det på ett liknande sätt som kommentarsidan.
     /**
      * Get the statistics from the database and convert the info to a json file for the javascript to read and build
      * a dynamic page based on.
