9fb5af3f851c6d5ccbc9c128772fec042a316018
Daniella Noren <daniellanoren@live.se>
2019-02-23 21:48:24 +0100

    Add some comments to classes, remove some old comments, rename HTTPresponseGenerator and Client

--- src/main/java/HTTPcommunication/HTTPRequestFactory.java
+++ src/main/java/HTTPcommunication/HTTPRequestFactory.java
@@ -3,7 +3,7 @@ package HTTPcommunication;
 import java.util.List;
 
 public class HTTPRequestFactory {
-
+    //Parse the Strings from a List containing HTTP-Headers into a HTTPRequest-object
     public static HTTPRequest getHTTPRequest(List<String> headersList){
 
         String connection = "";
@@ -12,19 +12,19 @@ public class HTTPRequestFactory {
         String accept = "";
         String query = "";
         String method = "";
-        String URL = "";
+        String path = "";
         String contentType = "";
 
         if(headersList != null && !(headersList.isEmpty())) {
-
+            //The first String in the List should contain the method and path
             method = headersList.get(0).substring(0, headersList.get(0).indexOf(" "));
-            URL = headersList.get(0).substring(headersList.get(0).indexOf(" ") + 1, headersList.get(0).lastIndexOf(" "));
-
-            if(URL.contains("?")) {
-                query = URL.substring(URL.indexOf("?")+1);
-                URL = URL.substring(0, URL.indexOf("?"));
+            path = headersList.get(0).substring(headersList.get(0).indexOf(" ") + 1, headersList.get(0).lastIndexOf(" "));
+            //Check if it has a query
+            if(path.contains("?")) {
+                query = path.substring(path.indexOf("?")+1);
+                path = path.substring(0, path.indexOf("?"));
             }
-
+            //Loop through the list, save relevant header-values
             for(String s : headersList){
                 switch(s.substring(0, s.indexOf(" ")).toLowerCase()){
                     case "connection:" : connection = s.substring(s.indexOf(" ")+1);
@@ -41,7 +41,7 @@ public class HTTPRequestFactory {
             }
         }
         return new HTTPRequest().setMethod(method).setConnection(connection).setContentLength(contentLength).setContentType(contentType)
-                .setHost(host).setQuery(query).setPath(URL);
+                .setHost(host).setQuery(query).setPath(path);
 
     }
 
